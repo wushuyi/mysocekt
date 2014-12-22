a51ffadbd4d40c93b088bdbc725fa2dade727ae4
巫书轶 <wushuyi541@gmail.com>
2014-12-22 11:39:06 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# websocket 画布传输
+
+websocket 画布传输
+
+实现 高效率的 画布 传输
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.all("*", function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Power-By", "wushuyi");
+    res.removeHeader("X-Powered-By");
+    next();
+});
+
+app.use(express.static(__dirname + "/apps"));
+
+io.on('connection', function (socket) {
+    socket.on('joinRoom', function(data){
+        socket.join(data.roomName);
+        console.log(socket.id);
+    });
+
+    socket.on('msgToSome', function (data) {
+        socket.broadcast.to(data.owner).emit('msgToSome', data.msg);
+    });
+
+    socket.on('board', function (name, data){
+        socket.broadcast.emit('board', name, data);
+    });
+
+    socket.on('getSocketId', function(data){
+        socket.emit('getSocketId', socket.id);
+    });
+
+    socket.on('getSocketRoom', function(data){
+        socket.emit('getSocketRoom', socket.rooms);
+    });
+});
